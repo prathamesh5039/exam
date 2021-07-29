bbdaeadc97d0a86eacc14b1ba152289a07b67024
root <root@localhost.localdomain>
2021-07-29 12:43:45 +0530

    First

---
+++ addition.c
@@ -0,0 +1,15 @@
+ int main() 
+    {
+            int m1, m2, sum;
+
+            printf("Enter the two integers are: ");
+            scanf("%d %d", &m1, &m2);
+
+            // calculating sum
+            sum = m1 + m2;
+
+            printf("%d + %d = %d", m1, m2, sum);
+            return 0;
+    }
+
+
